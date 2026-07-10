796fea357906a5f2f41ec45dea2153f48330aef3
REDHU DARSINI <redhudarsini3@gmail.com>
2026-07-10 12:25:29 +0530

    Updated

--- my-portfolio/src/component/ContactSection.jsx
+++ my-portfolio/src/component/ContactSection.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 import "./ContactSection.css";
 import ScrollReveal from "./ScrollReveal";
 
